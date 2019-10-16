f7b0f241ec2195794b226a008320b8919b30abfa
Tao <thomas.duditlieux@gmail.com>
2019-10-16 14:45:16 +0200

    a

---
+++ src/main/java/invoice/aaa.java
@@ -0,0 +1,14 @@
+package invoice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class aaa {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
